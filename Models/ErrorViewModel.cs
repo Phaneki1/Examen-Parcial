72a10cdcefd06c79a4dd531592b430c0b0a0a24e
Phaneki1 <phamixes05@gmail.com>
2026-04-25 11:34:15 -0500

    Inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Examen_Parcial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
